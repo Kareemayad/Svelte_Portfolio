ae851b874cda582ca0a9dd005e3684b3e0d3a8b6
Kareemayad <kareemayad@gmail.com>
2023-10-08 21:09:02 +0300

    init components make proper footer #11

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/components/layout/Header.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 </script>
 
 <div class="flex flex-col h-[100dvh] h-screen">
-	<div>h h</div>
+	<Header />
 	<div class="h-full">
 		<slot />
 	</div>
-	<div>f</div>
+	<Footer />
 </div>
