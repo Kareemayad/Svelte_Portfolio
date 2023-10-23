ee3e8d527e98c373406cb9d2d7cc832b0b1642e9
Kareemayad <kareemayad@gmail.com>
2023-10-24 01:45:52 +0300

    Make project cards page Fixes #13

--- src/routes/projectsPage/+page.svelte
+++ src/routes/projectsPage/+page.svelte
@@ -1,48 +1,5 @@
 <script lang="ts">
-	const dataArray = [
-		{
-			title: 'Project 1',
-			description:
-				'Breakout is a classic computer arcade game Demonstrate a practical understanding of Object-Oriented programming techniques Use sequence, selection and iteration to develop simple applications Utilize a variety of data types and collections Test and debug simple programs.',
-			src: 'BreakOut-game.png',
-			alt: 'Break Out Game'
-		},
-		{
-			title: 'Project 2',
-			description:
-				'Breakout is a classic computer arcade game Demonstrate a practical understanding of Object-Oriented programming techniques Use sequence, selection and iteration to develop simple applications Utilize a variety of data types and collections Test and debug simple programs.',
-			src: 'BreakOut-game.png',
-			alt: 'Break Out Game'
-		},
-		{
-			title: 'Project 3',
-			description:
-				'Breakout is a classic computer arcade game Demonstrate a practical understanding of Object-Oriented programming techniques Use sequence, selection and iteration to develop simple applications Utilize a variety of data types and collections Test and debug simple programs.',
-			src: 'BreakOut-game.png',
-			alt: 'Break Out Game'
-		},
-		{
-			title: 'Project 4',
-			description:
-				'Breakout is a classic computer arcade game Demonstrate a practical understanding of Object-Oriented programming techniques Use sequence, selection and iteration to develop simple applications Utilize a variety of data types and collections Test and debug simple programs.',
-			src: 'BreakOut-game.png',
-			alt: 'Break Out Game'
-		},
-		{
-			title: 'Project 5',
-			description:
-				'Breakout is a classic computer arcade game Demonstrate a practical understanding of Object-Oriented programming techniques Use sequence, selection and iteration to develop simple applications Utilize a variety of data types and collections Test and debug simple programs.',
-			src: 'BreakOut-game.png',
-			alt: 'Break Out Game'
-		},
-		{
-			title: 'Project 6',
-			description:
-				'Breakout is a classic computer arcade game Demonstrate a practical understanding of Object-Oriented programming techniques Use sequence, selection and iteration to develop simple applications Utilize a variety of data types and collections Test and debug simple programs.',
-			src: 'BreakOut-game.png',
-			alt: 'Break Out Game'
-		}
-	];
+	import dataArray from '$lib/data.json';
 </script>
 
 <div class="bg-gray-100 flex items-center justify-center w-full flex-col gap-20 py-10">
