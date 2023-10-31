1b6b021b23bc4a2a9da03031a99ed24d681a71d4
Kareemayad <kareemayad@gmail.com>
2023-10-31 07:38:04 +0200

    Make project cards page Fixes #13

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-row justify-between items-center bg-base-300">
+<div class="flex flex-row justify-between items-center bg-base-300 h-header">
 	<div class="p-3 font-bold flex text-2xl"><a href="/">KA</a></div>
 
 	<ul class="flex !flex-row justify-center gap-10 pr-28 menu menu-horizontal px-1 font-bold">
--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-full flex justify-center items-center flex-col gap-3 px-3 min-h-screen">
+<div class="w-full flex justify-center items-center flex-col gap-3 px-3">
 	<div class="avatar flex">
 		<div class=" w-full max-w-[150px] rounded-full">
 			<img alt="Sorty" src="Sorty.jpeg" />
@@ -8,7 +8,21 @@
 		<p>
 			I'm a recent graduate in Computer Science specializing in Artificial Intelligence from the
 			esteemed University of Brighton. Passionate about emerging technologies, I am committed to
-			leveraging AI to create solutions that drive efficiency and innovation.
+			leveraging AI to create solutions that drive efficiency and innovation. During my studies, I
+			had the opportunity to work on various projects that allowed me to hone my programming and
+			problem-solving skills. My hands-on experience includes working with languages such as Python,
+			Java, and C++, Html, Css, JavaScript, as well as frameworks like TensorFlow and PyTorch
+			Svelte, React, Vue. Some key highlights from my academic journey include: Leading a team
+			project to develop an AI-powered recommendation system that demonstrated a 20% improvement in
+			user engagement. Completing an internship at Juhayna, where I contributed to optimizing
+			machine learning algorithms for real-world applications. Publishing a research paper on
+			Natural Language Processing (NLP) in collaboration with my professors, focusing on sentiment
+			analysis in social media. I am constantly seeking opportunities to learn and grow within the
+			tech industry. Currently, I am open to entry-level positions where I can apply my knowledge in
+			AI and software development to create impactful solutions. I am particularly interested in
+			roles that allow for collaboration, creativity, and the chance to contribute to cutting-edge
+			projects. Feel free to connect with me or send a message if you'd like to discuss potential
+			opportunities, collaboration, or simply want to chat about the exciting world of AI!
 		</p>
 	</div>
 </div>
